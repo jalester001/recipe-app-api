f863ba001d8377a449f13829f44734c0db34afb6
jalester001 <jalester001@gmail.com>
2023-10-16 11:22:43 +0800

    Create test for wait_for_db

--- app/app/calc.py
+++ app/app/calc.py
@@ -10,10 +10,10 @@ or go to the directory of manage.py then run:
 # >>> python manage.py test
 """
 
-def add(x, y):
-    """Add x and y and return result."""
-    return x + y
+# def add(x, y):
+#     """Add x and y and return result."""
+#     return x + y
 
-def subtract(x, y):
-    """Subtract x and y and return result."""
-    return y - x
+# def subtract(x, y):
+#     """Subtract x and y and return result."""
+#     return y - x
--- app/app/test.py
+++ app/app/test.py
@@ -8,22 +8,22 @@ or go to the directory of manage.py then run:
 # >>> python manage.py test
 """
 
-from django.test import SimpleTestCase
+# from django.test import SimpleTestCase
 
-from app import calc
+# from app import calc
 
-class CalcTests(SimpleTestCase):
-    """Test the calc module"""
+# class CalcTests(SimpleTestCase):
+#     """Test the calc module"""
 
-    def test_add_numbers(self):
-        """Test adding numbers together"""
-        res = calc.add(5, 6)
+#     def test_add_numbers(self):
+#         """Test adding numbers together"""
+#         res = calc.add(5, 6)
 
-        self.assertEqual(res, 11)
+#         self.assertEqual(res, 11)
 
-    def test_subtract_numbers(self):
-        """Test subtracting numbers"""
+#     def test_subtract_numbers(self):
+#         """Test subtracting numbers"""
 
-        res = calc.subtract(10, 15)
+#         res = calc.subtract(10, 15)
 
-        self.assertEqual(res, 5)
+#         self.assertEqual(res, 5)
